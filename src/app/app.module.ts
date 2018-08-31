f8e245b6063829201eab1e5ef7385da78815b5c7
IamGiel <november14giel@gmail.com>
2018-08-31 11:05:48 -0400

    two way d=binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { ExponentialStrengthPipe } from "./exponentialStrength.pipe";
+import { AppComponent } from "./app.component";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { PowerBoosterComponent } from "./powerbooster.component";
+import { PowerBoostCalculatorComponent } from "./powerbooster-calculator.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    ExponentialStrengthPipe,
+    PowerBoosterComponent,
+    PowerBoostCalculatorComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,7 +12,18 @@ export class HeroesComponent implements OnInit {
   constructor() {}
   hero: Hero = {
     id: 1,
-    name: "Windstorm"
+    name: "Hero's Name",
+    birthday: new Date("1988, 3, 15")
   };
+  get format() {
+    return this.toggle ? "shortDate" : "fullDate";
+  }
+  toggleFormat() {
+    this.toggle = !this.toggle;
+  }
+
+  power = 5;
+  factor = 1;
+
   ngOnInit() {}
 }
